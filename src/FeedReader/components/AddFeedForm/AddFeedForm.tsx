bafefa010ac5ad88846c4f8c50f06202f741f4bc
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-04-03 00:27:01 +0200

    Refactor feed registering

--- src/FeedReader/components/AddFeedForm/AddFeedForm.tsx
+++ src/FeedReader/components/AddFeedForm/AddFeedForm.tsx
@@ -1,16 +1,11 @@
-import { useRef } from "react";
 import { useForm } from "react-hook-form";
-import { dispatch, feedReaderApi } from "src/store/config";
-import { useSelector } from "src/store";
 import * as styles from "./AddFeedForm.css";
+import { useRegisterFeed } from "src/FeedReader/hooks";
 
 const FEED_URL = "feedUrl";
 
 export default function AddFeedForm() {
-  const ref = useRef("");
-  const mutationState = useSelector(
-    feedReaderApi.endpoints.registerFeed.select(ref.current)
-  );
+  const [mutate, { isLoading }] = useRegisterFeed();
 
   const {
     register,
@@ -25,14 +20,7 @@ export default function AddFeedForm() {
     <form
       onSubmit={handleSubmit(async (data) => {
         try {
-          const { requestId, unwrap } = dispatch(
-            feedReaderApi.endpoints.registerFeed.initiate(data.feedUrl, {
-              track: true,
-            })
-          );
-          data.feedUrl;
-          ref.current = requestId;
-          await unwrap();
+          await mutate(data.feedUrl).unwrap();
           reset();
           alert("Feed added successfully");
         } catch (e) {
@@ -58,11 +46,7 @@ export default function AddFeedForm() {
           </p>
         )}
       </div>
-      <button
-        className={styles.button}
-        disabled={mutationState.isLoading}
-        type="submit"
-      >
+      <button className={styles.button} disabled={isLoading} type="submit">
         Register feed
       </button>
     </form>
--- src/FeedReader/hooks.ts
+++ src/FeedReader/hooks.ts
@@ -1,4 +1,4 @@
-import { useEffect } from "react";
+import { useCallback, useEffect, useRef } from "react";
 import { useSelector } from "src/store";
 import { retrieveFeedList } from "src/FeedReader/queries/retrieveFeedList";
 import { dispatch, feedReaderApi } from "src/store/config";
@@ -16,4 +16,16 @@ export function useRetrieveFeedList<T>(selectCallback?: (state?: Feed[]) => T) {
     const data = selectCallback?.(raw.data);
     return { ...raw, selectorResult: data };
   });
+}
+
+export function useRegisterFeed() {
+  const requestIdRef = useRef("");
+  const mutationState = useSelector(
+    feedReaderApi.endpoints.registerFeed.select(requestIdRef.current)
+  );
+  const mutate = useCallback(function (url: string) {
+    requestIdRef.current = url;
+    return dispatch(feedReaderApi.endpoints.registerFeed.initiate(url));
+  }, []);
+  return [mutate, mutationState] as const;
 }
