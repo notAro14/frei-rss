df9e2d59c74fd7e5f47c052e483dff1fc0dbe24a
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-05-02 20:36:16 +0200

    Add OFFLINE dev

--- src/store/config.ts
+++ src/store/config.ts
@@ -1,10 +1,29 @@
 import { setupStore } from "src/store";
 import { setupRootApi } from "src/store/root.api";
 import { FeedReaderProductionGateway } from "src/Feed/gateways/FeedReaderProduction.gateway";
+import { FeedReaderInMemoryGateway } from "src/Feed/gateways/FeedReaderInMemory.gateway";
 import { setupFeedReaderApi } from "src/FeedReader/api";
 
+const OFFLINE = process.env.OFFLINE;
+
 const rootApi = setupRootApi();
-const feedReaderGateway = new FeedReaderProductionGateway();
+const feedReaderGateway = OFFLINE
+  ? new FeedReaderInMemoryGateway([
+      {
+        id: "1",
+        website: "https://example.com/",
+        name: "Example Dot Com",
+        feedItems: [
+          {
+            id: "11",
+            title: "Hello World Article",
+            url: "https://example.com/hello-world/",
+            date: "2023-01-01",
+          },
+        ],
+      },
+    ])
+  : new FeedReaderProductionGateway();
 export const store = setupStore({ rootApi, feedReaderGateway });
 
 export const feedReaderApi = setupFeedReaderApi(rootApi);
