f1811051ad622bf28d0edd752b08bf6c938d633b
Aro Andriamaro <mamitiana.dev.js@gmail.com>
2023-05-08 22:48:08 +0200

    Implement undo

--- src/domain/Feed/usecases/removeFeed/removeFeed.reducer.ts
+++ src/domain/Feed/usecases/removeFeed/removeFeed.reducer.ts
@@ -1,12 +1,8 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { removeFeed } from "./removeFeed";
-
-export interface RemoveFeed {
-  status: "pending" | "idle" | "fulfilled" | "rejected";
-}
+import { createSlice, createAction } from "@reduxjs/toolkit";
 
 export const initialState: RemoveFeed = {
   status: "idle",
+  feedToRemove: {},
 };
 
 export const removeFeedSlice = createSlice({
@@ -14,14 +10,56 @@ export const removeFeedSlice = createSlice({
   initialState,
   reducers: {},
   extraReducers(builder) {
-    builder.addCase(removeFeed.pending, (state) => {
+    // builder.addCase(removeFeed.pending, (state) => {
+    //   state.status = "pending";
+    // });
+    // builder.addCase(removeFeed.fulfilled, (state) => {
+    //   state.status = "fulfilled";
+    // });
+    // builder.addCase(removeFeed.rejected, (state) => {
+    //   state.status = "rejected";
+    // });
+
+    builder.addCase(removeFeedInit, (state, action) => {
+      const { feedId, timerId } = action.payload;
+      state.feedToRemove = {
+        ...state.feedToRemove,
+        [feedId]: {
+          id: feedId,
+          timerId,
+        },
+      };
       state.status = "pending";
     });
-    builder.addCase(removeFeed.fulfilled, (state) => {
+    builder.addCase(removeFeedCancel, (state, action) => {
+      const { feedId } = action.payload;
+      clearTimeout(state.feedToRemove[feedId].timerId);
+      delete state.feedToRemove[feedId];
       state.status = "fulfilled";
     });
-    builder.addCase(removeFeed.rejected, (state) => {
-      state.status = "rejected";
+    builder.addCase(removeFeedDone, (state, action) => {
+      delete state.feedToRemove[action.payload.feedId];
+      state.status = "fulfilled";
     });
   },
 });
+
+export interface RemoveFeed {
+  status: "pending" | "idle" | "fulfilled" | "rejected";
+  feedToRemove: {
+    [key: string]: {
+      id: string;
+      timerId: NodeJS.Timeout;
+    };
+  };
+}
+export const removeFeedInit = createAction<{
+  feedId: string;
+  timerId: NodeJS.Timeout;
+}>("feed/removeFeed/init");
+export const removeFeedCancel = createAction<{ feedId: string }>(
+  "feed/removeFeed/cancel"
+);
+export const removeFeedDone = createAction<{ feedId: string }>(
+  "feed/removeFeed/done"
+);
--- src/domain/Feed/usecases/removeFeed/removeFeed.ts
+++ src/domain/Feed/usecases/removeFeed/removeFeed.ts
@@ -1,19 +1,35 @@
 import { createAppAsyncThunk } from "src/store/thunk";
+import {
+  removeFeedInit,
+  removeFeedDone,
+  removeFeedCancel,
+} from "./removeFeed.reducer";
 
-export const removeFeed = createAppAsyncThunk<
-  { feedId: string },
-  { feedId: string }
->("feed/removeFeed", async function ({ feedId }, { extra, rejectWithValue }) {
-  const {
-    dependencies: { feedReaderGateway },
-  } = extra;
-  if (!feedReaderGateway?.deleteFeed)
-    throw new Error("FeedReaderGateway.deleteFeed is not defined");
+export const removeFeed = createAppAsyncThunk<void, { feedId: string }>(
+  "feed/removeFeed",
+  function ({ feedId }, { extra, rejectWithValue, dispatch }) {
+    const {
+      dependencies: { feedReaderGateway },
+    } = extra;
+    if (!feedReaderGateway?.deleteFeed)
+      throw new Error("FeedReaderGateway.deleteFeed is not defined");
 
-  try {
-    const feed = await feedReaderGateway.deleteFeed(feedId);
-    return feed;
-  } catch (e) {
-    return rejectWithValue("Could not remove feed");
+    const timerId = setTimeout(async () => {
+      try {
+        await feedReaderGateway?.deleteFeed?.(feedId);
+        dispatch(removeFeedDone({ feedId }));
+      } catch (e) {
+        dispatch(removeFeedCancel({ feedId }));
+        rejectWithValue("Could not remove feed");
+      }
+    }, 5000);
+    dispatch(removeFeedInit({ feedId, timerId }));
+
+    // try {
+    //   const feed = await feedReaderGateway.deleteFeed(feedId);
+    //   return feed;
+    // } catch (e) {
+    //   return rejectWithValue("Could not remove feed");
+    // }
   }
-});
+);
