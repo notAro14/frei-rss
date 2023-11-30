02e8374dbfbfc0506fec9617e808177ebc6abf0e
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2023-11-30 19:04:35 +0100

    refactor JSX

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -27,82 +27,95 @@ export default function HomePage() {
   useGetFeeds();
   return (
     <Container size={"2"} p={"4"}>
-      <Flex justify={"between"} mt={"4"}>
-        <Flex gap={"4"} align={"baseline"}>
-          <Heading
-            as="h1"
-            id="logo"
-            size={{ initial: "6", xs: "8" }}
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "var(--space-2)",
-            }}
-          >
-            <Rss size={"1em"} /> FreiRSS
-          </Heading>
-          <Separator orientation="vertical" />
-          <RxLink asChild>
-            <Link href={"#navigation"}>Menu</Link>
-          </RxLink>
-        </Flex>
-
-        <Button
-          color="crimson"
-          variant="outline"
-          onClick={async () => {
-            await supabase.auth.signOut();
-          }}
-        >
-          Sign out
-        </Button>
-      </Flex>
+      <Header />
       <AddFeedForm />
-      <Heading as="h2" mb={"4"}>
-        This Month articles
-      </Heading>
       <ThisMonthArticles />
       <Separator size={"3"} mx={"auto"} my={"8"} />
-      <Box asChild>
-        <footer>
-          <Heading
-            style={{
-              display: "flex",
-              alignItems: "center",
-              gap: "var(--space-2)",
-            }}
-            as="h2"
-            id="navigation"
-          >
-            <Signpost size={"1em"} /> Menu
-          </Heading>
-          <Flex direction={"column"} mt={"4"} asChild gap={"2"}>
-            <nav>
-              <RxLink asChild>
-                <Link href={"#"}>All feeds</Link>
-              </RxLink>
-              <RxLink asChild>
-                <Link href={"/"}>This Month articles</Link>
-              </RxLink>
-              <RxLink asChild>
-                <Link href={"#"}>Unread articles</Link>
-              </RxLink>
-              <Separator size={"4"} my={"4"} />
-              <RxLink
-                underline="always"
-                style={{ alignSelf: "flex-end" }}
-                asChild
-              >
-                <Link href={"#logo"}>
-                  <MoveUp size={"1em"} />
-                  Top
-                </Link>
-              </RxLink>
-            </nav>
-          </Flex>
-        </footer>
-      </Box>
+      <Footer />
     </Container>
+  );
+}
+
+/* ---------- Header ---------- */
+
+function Header() {
+  return (
+    <Flex justify={"between"} mt={"4"}>
+      <Flex gap={"4"} align={"baseline"}>
+        <Heading
+          as="h1"
+          id="logo"
+          size={{ initial: "6", xs: "8" }}
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "var(--space-2)",
+          }}
+        >
+          <Rss size={"1em"} /> FreiRSS
+        </Heading>
+        <Separator orientation="vertical" />
+        <RxLink asChild>
+          <Link href={"#navigation"}>Menu</Link>
+        </RxLink>
+      </Flex>
+
+      <Button
+        color="crimson"
+        variant="outline"
+        onClick={async () => {
+          await supabase.auth.signOut();
+        }}
+      >
+        Sign out
+      </Button>
+    </Flex>
+  );
+}
+
+/* ---------- Footer ---------- */
+
+function Footer() {
+  return (
+    <Box asChild>
+      <footer>
+        <Heading
+          style={{
+            display: "flex",
+            alignItems: "center",
+            gap: "var(--space-2)",
+          }}
+          as="h2"
+          id="navigation"
+        >
+          <Signpost size={"1em"} /> Menu
+        </Heading>
+        <Flex direction={"column"} mt={"4"} asChild gap={"2"}>
+          <nav>
+            <RxLink asChild>
+              <Link href={"#"}>All feeds</Link>
+            </RxLink>
+            <RxLink asChild>
+              <Link href={"/"}>This Month articles</Link>
+            </RxLink>
+            <RxLink asChild>
+              <Link href={"#"}>Unread articles</Link>
+            </RxLink>
+            <Separator size={"4"} my={"4"} />
+            <RxLink
+              underline="always"
+              style={{ alignSelf: "flex-end" }}
+              asChild
+            >
+              <Link href={"#logo"}>
+                <MoveUp size={"1em"} />
+                Top
+              </Link>
+            </RxLink>
+          </nav>
+        </Flex>
+      </footer>
+    </Box>
   );
 }
 
