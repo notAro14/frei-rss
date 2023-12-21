b338ff005fe9d018abd1f5a7afe9441f580c3861
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2023-12-21 15:26:52 +0100

    migrate favicons to app router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,11 @@ import { ReactNode } from "react";
 import { Providers } from "src/components/Providers";
 import { Container } from "@radix-ui/themes";
 import { Header } from "src/components/Header";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Frei RSS",
+};
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
