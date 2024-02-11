ada7bcd561a0f5e0c71883e7156cb5d7c379e48e
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-02-11 19:40:23 +0100

    gen supabase types

--- src/types/supabase.ts
+++ src/types/supabase.ts
@@ -4,111 +4,117 @@ export type Json =
   | boolean
   | null
   | { [key: string]: Json | undefined }
-  | Json[];
+  | Json[]
 
-export interface Database {
+export type Database = {
   public: {
     Tables: {
       feed_items: {
         Row: {
-          content: string;
-          created_at: string | null;
-          fk_feed_id: string;
-          id: string;
-          is_read: boolean | null;
-          pub_date: string;
-          title: string;
-          url: string;
-          user_id: string;
-        };
+          content: string
+          created_at: string | null
+          favorite: boolean | null
+          fk_feed_id: string
+          id: string
+          is_read: boolean | null
+          pub_date: string
+          status: Database["public"]["Enums"]["feed_item_status"] | null
+          title: string
+          url: string
+          user_id: string
+        }
         Insert: {
-          content?: string;
-          created_at?: string | null;
-          fk_feed_id: string;
-          id?: string;
-          is_read?: boolean | null;
-          pub_date: string;
-          title: string;
-          url: string;
-          user_id: string;
-        };
+          content?: string
+          created_at?: string | null
+          favorite?: boolean | null
+          fk_feed_id: string
+          id?: string
+          is_read?: boolean | null
+          pub_date: string
+          status?: Database["public"]["Enums"]["feed_item_status"] | null
+          title: string
+          url: string
+          user_id: string
+        }
         Update: {
-          content?: string;
-          created_at?: string | null;
-          fk_feed_id?: string;
-          id?: string;
-          is_read?: boolean | null;
-          pub_date?: string;
-          title?: string;
-          url?: string;
-          user_id?: string;
-        };
+          content?: string
+          created_at?: string | null
+          favorite?: boolean | null
+          fk_feed_id?: string
+          id?: string
+          is_read?: boolean | null
+          pub_date?: string
+          status?: Database["public"]["Enums"]["feed_item_status"] | null
+          title?: string
+          url?: string
+          user_id?: string
+        }
         Relationships: [
           {
-            foreignKeyName: "feed_items_fk_feed_id_fkey";
-            columns: ["fk_feed_id"];
-            isOneToOne: false;
-            referencedRelation: "feeds";
-            referencedColumns: ["id"];
+            foreignKeyName: "feed_items_fk_feed_id_fkey"
+            columns: ["fk_feed_id"]
+            isOneToOne: false
+            referencedRelation: "feeds"
+            referencedColumns: ["id"]
           },
           {
-            foreignKeyName: "feed_items_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
-          },
-        ];
-      };
+            foreignKeyName: "feed_items_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
       feeds: {
         Row: {
-          created_at: string | null;
-          id: string;
-          name: string | null;
-          url: string;
-          user_id: string;
-          website: string | null;
-        };
+          created_at: string | null
+          id: string
+          name: string | null
+          url: string
+          user_id: string
+          website: string | null
+        }
         Insert: {
-          created_at?: string | null;
-          id?: string;
-          name?: string | null;
-          url: string;
-          user_id: string;
-          website?: string | null;
-        };
+          created_at?: string | null
+          id?: string
+          name?: string | null
+          url: string
+          user_id: string
+          website?: string | null
+        }
         Update: {
-          created_at?: string | null;
-          id?: string;
-          name?: string | null;
-          url?: string;
-          user_id?: string;
-          website?: string | null;
-        };
+          created_at?: string | null
+          id?: string
+          name?: string | null
+          url?: string
+          user_id?: string
+          website?: string | null
+        }
         Relationships: [
           {
-            foreignKeyName: "feeds_user_id_fkey";
-            columns: ["user_id"];
-            isOneToOne: false;
-            referencedRelation: "users";
-            referencedColumns: ["id"];
-          },
-        ];
-      };
-    };
+            foreignKeyName: "feeds_user_id_fkey"
+            columns: ["user_id"]
+            isOneToOne: false
+            referencedRelation: "users"
+            referencedColumns: ["id"]
+          }
+        ]
+      }
+    }
     Views: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Functions: {
-      [_ in never]: never;
-    };
+      [_ in never]: never
+    }
     Enums: {
-      [_ in never]: never;
-    };
+      feed_item_status: "read" | "unread" | "read_later"
+    }
     CompositeTypes: {
-      [_ in never]: never;
-    };
-  };
+      [_ in never]: never
+    }
+  }
 }
 
 export type Tables<
@@ -118,23 +124,23 @@ export type Tables<
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
         Database[PublicTableNameOrOptions["schema"]]["Views"])
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? (Database[PublicTableNameOrOptions["schema"]]["Tables"] &
       Database[PublicTableNameOrOptions["schema"]]["Views"])[TableName] extends {
-      Row: infer R;
+      Row: infer R
     }
     ? R
     : never
   : PublicTableNameOrOptions extends keyof (Database["public"]["Tables"] &
-        Database["public"]["Views"])
-    ? (Database["public"]["Tables"] &
-        Database["public"]["Views"])[PublicTableNameOrOptions] extends {
-        Row: infer R;
-      }
-      ? R
-      : never
-    : never;
+      Database["public"]["Views"])
+  ? (Database["public"]["Tables"] &
+      Database["public"]["Views"])[PublicTableNameOrOptions] extends {
+      Row: infer R
+    }
+    ? R
+    : never
+  : never
 
 export type TablesInsert<
   PublicTableNameOrOptions extends
@@ -142,20 +148,20 @@ export type TablesInsert<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Insert: infer I;
+      Insert: infer I
     }
     ? I
     : never
   : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-        Insert: infer I;
-      }
-      ? I
-      : never
-    : never;
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Insert: infer I
+    }
+    ? I
+    : never
+  : never
 
 export type TablesUpdate<
   PublicTableNameOrOptions extends
@@ -163,20 +169,20 @@ export type TablesUpdate<
     | { schema: keyof Database },
   TableName extends PublicTableNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicTableNameOrOptions["schema"]]["Tables"]
-    : never = never,
+    : never = never
 > = PublicTableNameOrOptions extends { schema: keyof Database }
   ? Database[PublicTableNameOrOptions["schema"]]["Tables"][TableName] extends {
-      Update: infer U;
+      Update: infer U
     }
     ? U
     : never
   : PublicTableNameOrOptions extends keyof Database["public"]["Tables"]
-    ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
-        Update: infer U;
-      }
-      ? U
-      : never
-    : never;
+  ? Database["public"]["Tables"][PublicTableNameOrOptions] extends {
+      Update: infer U
+    }
+    ? U
+    : never
+  : never
 
 export type Enums<
   PublicEnumNameOrOptions extends
@@ -184,9 +190,9 @@ export type Enums<
     | { schema: keyof Database },
   EnumName extends PublicEnumNameOrOptions extends { schema: keyof Database }
     ? keyof Database[PublicEnumNameOrOptions["schema"]]["Enums"]
-    : never = never,
+    : never = never
 > = PublicEnumNameOrOptions extends { schema: keyof Database }
   ? Database[PublicEnumNameOrOptions["schema"]]["Enums"][EnumName]
   : PublicEnumNameOrOptions extends keyof Database["public"]["Enums"]
-    ? Database["public"]["Enums"][PublicEnumNameOrOptions]
-    : never;
+  ? Database["public"]["Enums"][PublicEnumNameOrOptions]
+  : never
