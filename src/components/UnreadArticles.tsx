9406a2aa3cddaf17d8eed59504a482ae705f9796
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-02-18 18:45:22 +0100

    use react virtual for unread articles

--- src/components/UnreadArticles.tsx
+++ src/components/UnreadArticles.tsx
@@ -1,22 +1,62 @@
 "use client";
 
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef } from "react";
 import { useSelector } from "src/store";
 import { getUnreadArticleIds } from "src/selectors/getUnreadArticleIds.selector";
 import { Article } from "src/components/Article";
 import { Loader } from "src/components/Loader";
-import { useInfiniteScrollItemsLoad } from "src/hooks/useInfiniteScrollItemsLoad";
 
 export function UnreadArticles() {
   const unreadFeedItemIds = useSelector(getUnreadArticleIds);
-  const { visible, ref } = useInfiniteScrollItemsLoad(unreadFeedItemIds);
 
   if (!unreadFeedItemIds) return <Loader />;
+  return <UnreadArticlesInner ids={unreadFeedItemIds} />;
+}
+
+export function UnreadArticlesInner({ ids }: { ids: string[] }) {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const virtualizer = useVirtualizer({
+    count: ids.length,
+    getScrollElement: () => parentRef.current,
+    estimateSize: () => 175,
+  });
+  const items = virtualizer.getVirtualItems();
   return (
-    <>
-      {visible.map((id) => {
-        return <Article key={id} id={id} />;
-      })}
-      <div ref={ref} />
-    </>
+    <div
+      style={{
+        height: "65lvh",
+        contain: "strict",
+        scrollbarGutter: "stable",
+      }}
+      className="max-w-full overflow-y-auto overflow-x-hidden px-2"
+      ref={parentRef}
+    >
+      <div
+        style={{
+          height: virtualizer.getTotalSize(),
+          position: "relative",
+        }}
+      >
+        <div
+          style={{
+            transform: `translateY(${items[0]?.start ?? 0}px)`,
+          }}
+          className="absolute left-0 top-0"
+        >
+          {items.map((virtualRow) => {
+            const id = ids[virtualRow.index];
+            return (
+              <Article
+                key={virtualRow.key}
+                dataIndex={virtualRow.index}
+                ref={virtualizer.measureElement}
+                id={id}
+              />
+            );
+          })}
+        </div>
+      </div>
+    </div>
   );
 }
