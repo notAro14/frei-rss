4580dec72f36a841577683be619c9925075c93e0
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-02-18 19:53:32 +0100

    add scroll to top and bottom to unread

--- src/components/UnreadArticles.tsx
+++ src/components/UnreadArticles.tsx
@@ -5,6 +5,8 @@ import { getUnreadArticleIds } from "src/selectors/getUnreadArticleIds.selector"
 import { Article } from "src/components/Article";
 import { Loader } from "src/components/Loader";
 import { useVirtual } from "src/hooks/useVirtual";
+import { Button } from "@radix-ui/themes";
+import { ArrowDown, ArrowUp } from "lucide-react";
 
 export function UnreadArticles() {
   const unreadFeedItemIds = useSelector(getUnreadArticleIds);
@@ -14,25 +16,52 @@ export function UnreadArticles() {
 }
 
 export function UnreadArticlesInner({ ids }: { ids: string[] }) {
+  const count = ids.length;
   const { items, virtualizer, div1Props, div2Props, div3Props } = useVirtual({
-    count: ids.length,
+    count,
   });
+
   return (
-    <div {...div1Props}>
-      <div {...div2Props}>
-        <div {...div3Props}>
-          {items.map((virtualRow) => {
-            const id = ids[virtualRow.index];
-            return (
-              <Article
-                key={virtualRow.key}
-                dataIndex={virtualRow.index}
-                ref={virtualizer.measureElement}
-                id={id}
-              />
-            );
-          })}
+    <div className="flex flex-col gap-4">
+      <header className="flex flex-wrap items-center gap-4">
+        <Button
+          variant="ghost"
+          size={"1"}
+          onClick={() => {
+            virtualizer.scrollToIndex(0);
+          }}
+        >
+          <ArrowUp size={"1em"} />
+          Scroll to top
+        </Button>
+        <Button
+          variant="ghost"
+          size={"1"}
+          onClick={() => {
+            virtualizer.scrollToIndex(count - 1);
+          }}
+        >
+          <ArrowDown size={"1em"} />
+          Scroll to bottom
+        </Button>
+      </header>
+      <div {...div1Props}>
+        <div {...div2Props}>
+          <div {...div3Props}>
+            {items.map((virtualRow) => {
+              const id = ids[virtualRow.index];
+              return (
+                <Article
+                  key={virtualRow.key}
+                  dataIndex={virtualRow.index}
+                  ref={virtualizer.measureElement}
+                  id={id}
+                />
+              );
+            })}
+          </div>
         </div>
+        he
       </div>
     </div>
   );
