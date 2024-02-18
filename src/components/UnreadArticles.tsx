e30d41b99ac4ee2756d1afc55dfcf87558fbe275
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-02-18 19:20:32 +0100

    move useVirtual on its own file

--- src/components/UnreadArticles.tsx
+++ src/components/UnreadArticles.tsx
@@ -1,11 +1,10 @@
 "use client";
 
-import { useVirtualizer } from "@tanstack/react-virtual";
-import { useRef } from "react";
 import { useSelector } from "src/store";
 import { getUnreadArticleIds } from "src/selectors/getUnreadArticleIds.selector";
 import { Article } from "src/components/Article";
 import { Loader } from "src/components/Loader";
+import { useVirtual } from "src/hooks/useVirtual";
 
 export function UnreadArticles() {
   const unreadFeedItemIds = useSelector(getUnreadArticleIds);
@@ -38,43 +37,4 @@ export function UnreadArticlesInner({ ids }: { ids: string[] }) {
       </div>
     </div>
   );
-}
-
-function useVirtual({
-  count,
-  estimateSize = () => 175,
-}: {
-  count: number;
-  estimateSize: () => number;
-}) {
-  const parentRef = useRef<HTMLDivElement>(null);
-  const virtualizer = useVirtualizer({
-    count,
-    getScrollElement: () => parentRef.current,
-    estimateSize,
-  });
-  const items = virtualizer.getVirtualItems();
-
-  const div1Props = {
-    style: {
-      height: "65lvh",
-      contain: "strict",
-      scrollbarGutter: "stable",
-    },
-    className: "max-w-full overflow-y-auto overflow-x-hidden px-2",
-    ref: parentRef,
-  } as const;
-  const div2Props = {
-    style: {
-      height: virtualizer.getTotalSize(),
-      position: "relative",
-    },
-  } as const;
-  const div3Props = {
-    style: {
-      transform: `translateY(${items[0]?.start ?? 0}px)`,
-    },
-    className: "absolute left-0 top-0",
-  } as const;
-  return { virtualizer, items, div1Props, div2Props, div3Props };
 }
---
+++ src/hooks/useVirtual.ts
@@ -0,0 +1,41 @@
+import { useVirtualizer } from "@tanstack/react-virtual";
+import { useRef } from "react";
+
+export function useVirtual({
+  count,
+  estimateSize = () => 175,
+}: {
+  count: number;
+  estimateSize: () => number;
+}) {
+  const parentRef = useRef<HTMLDivElement>(null);
+  const virtualizer = useVirtualizer({
+    count,
+    getScrollElement: () => parentRef.current,
+    estimateSize,
+  });
+  const items = virtualizer.getVirtualItems();
+
+  const div1Props = {
+    style: {
+      height: "65lvh",
+      contain: "strict",
+      scrollbarGutter: "stable",
+    },
+    className: "max-w-full overflow-y-auto overflow-x-hidden px-2",
+    ref: parentRef,
+  } as const;
+  const div2Props = {
+    style: {
+      height: virtualizer.getTotalSize(),
+      position: "relative",
+    },
+  } as const;
+  const div3Props = {
+    style: {
+      transform: `translateY(${items[0]?.start ?? 0}px)`,
+    },
+    className: "absolute left-0 top-0",
+  } as const;
+  return { virtualizer, items, div1Props, div2Props, div3Props };
+}
