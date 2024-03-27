f6fbcb28ede272bc6b2c727588ad08bd5208c932
Aro Andriamaro <47597252+notAro14@users.noreply.github.com>
2024-03-27 23:23:48 +0100

    change skeleton model

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,10 +1,12 @@
 import styles from "./Loader.module.scss";
+import clsx from "clsx";
 
 export function Loader() {
   return (
-    <div role="progress" className="grid grid-cols-2 gap-2">
-      <div className={styles.loader} />
-      <div className={styles.loader} />
+    <div role="progress" className={clsx("grid h-[200px] gap-2", styles.root)}>
+      <div className={clsx(styles.loader, styles["loader-1"])} />
+      <div className={clsx(styles.loader, styles["loader-2"])} />
+      <div className={clsx(styles.loader, styles["loader-3"])} />
     </div>
   );
 }
